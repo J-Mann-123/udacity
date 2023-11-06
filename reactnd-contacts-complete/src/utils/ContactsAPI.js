a221934962dc33e29e1d6bb126730bbf4c8d8c45
J-Mann-123 <jmann5326@gmail.com>
2023-11-06 18:04:29 +1030

    First commit

---
+++ reactnd-contacts-complete/src/utils/ContactsAPI.js
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://reactnd-env.eba-9werppqp.us-east-2.elasticbeanstalk.com'
+=======
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://reactnd-contacts-complete-dev.us-east-1.elasticbeanstalk.com'
+>>>>>>> 64e2534a698eb94bd79770e965f15799f0243f10
+
+let token = localStorage.token
+
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+export const getAll = () =>
+  fetch(`${api}/contacts`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+
+export const remove = (contact) =>
+  fetch(`${api}/contacts/${contact.id}`, { method: 'DELETE', headers })
+    .then(res => res.json())
+    .then(data => data.contact)
+
+export const create = (body) =>
+  fetch(`${api}/contacts`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }).then(res => res.json())
